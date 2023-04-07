999de3ef812ba307895534134ff3f2355b4a5b11
Beatriz Maia <beamaia.work@gmail.com>
2023-04-07 20:29:56 -0300

    add suffix to file name
    Co-Authored-By: Sophie Dilhon <51868657+AHalic@users.noreply.github.com>
    Co-Authored-By: IagoSC <21135637+IagoSC@users.noreply.github.com>

--- Lab_1/report.py
+++ Lab_1/report.py
@@ -5,25 +5,27 @@ from datetime import datetime
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def generate_report(n_threads: int, array_size: int, start_time: float, end_time: float) -> None:
+def generate_report(n: int, array_size: int, start_time: float, end_time: float, suffix: str) -> None:
     """
     Generates a report in CSV format
-    @param n_threads: Number of threads
+    @param n: Number of threads
     @param array_size: Size of the array
     @param start_time: Start time of the execution
     @param end_time: End time of the execution
     return: None
     """
     info = {
-        "n_threads": n_threads,
+        "n": n,
         "array_size": array_size,
         "time_elapsed": (end_time - start_time) * 1000, # in milliseconds
         "time": datetime.now().strftime("%d/%m/%Y %H:%M:%S")
     }
 
-    write_header = not os.path.exists(r'report.csv')
+    file = f'{suffix}_report.csv'
 
-    with open(r'report.csv', 'a', newline='') as csvfile:
+    write_header = not os.path.exists(file)
+
+    with open(file, 'a', newline='') as csvfile:
         fieldnames = list(info.keys())
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter=';')
         
@@ -39,5 +41,5 @@ def scatter_plot_report() -> None:
     """
 
     df = pd.read_csv(r'report.csv', delimiter=';')
-    df.plot(x='n_threads', y='time_elapsed', kind='scatter')
+    df.plot(x='n', y='time_elapsed', kind='scatter')
     plt.show()	
