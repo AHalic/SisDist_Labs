fb83aaa28c4d941541142b4244358c5f07768f47
iago cerqueira <iagocerqueira@velocityglobal.com>
2023-04-07 19:45:24 -0300

    Add main arg use_process

--- Lab_1/main.py
+++ Lab_1/main.py
@@ -5,17 +5,18 @@ from custom_thread import CustomThread
 from arrays import create_array, divide_array, sort_arrays_threads, merge_arrays_threads, sort_arrays_processes, merge_arrays_processes
 from report import generate_report
 
-def parse_args() -> tuple[int, int]:
+def parse_args() -> tuple[int, int, int]:
     parser = argparse.ArgumentParser(description='Merge sort with threads')
     parser.add_argument('n_threads', type=int, help='Number of threads')
     parser.add_argument('array_size', type=int, help='Size of the array')
+    parser.add_argument('use_process', type=int, help='Should use process instead of threads?')
     args = parser.parse_args()    
 
-    return args.n_threads, args.array_size
+    return args.n_threads, args.array_size, use_process
 
 if __name__ == "__main__":
     # Parse arguments
-    n_threads, array_size = parse_args()
+    n_threads, array_size, use_process = parse_args()
 
     # Create array
     array = create_array(array_size)
@@ -27,13 +28,17 @@ if __name__ == "__main__":
 
 
     # Sort arrays using threads
-    # sorted_arrays = sort_arrays_threads(n_threads, arrays)
-    sorted_arrays = sort_arrays_processes(n_threads, arrays)
+    if use_process:
+      sorted_arrays = sort_arrays_processes(n_threads, arrays)
+    else: 
+      sorted_arrays = sort_arrays_threads(n_threads, arrays)
 
     # Merge arrays
     while len(sorted_arrays) > 1:
-        sorted_arrays = merge_arrays_processes(sorted_arrays)
-        # sorted_arrays = merge_arrays_threads(sorted_arrays)
+          if use_process:
+            sorted_arrays = merge_arrays_processes(sorted_arrays)
+          else: 
+            sorted_arrays = merge_arrays_threads(sorted_arrays)
     
     # Stop timer
     end_time = time.time()
