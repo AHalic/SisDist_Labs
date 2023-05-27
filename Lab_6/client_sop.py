1cbc6d7329677b8e936008f7e60f5d5cdf4b389b
AHalic <sophie.gama@aluno.ufes.br>
2023-05-27 16:12:12 -0300

    :sparkles: starts challenge + change file names

--- Lab_6/client_sop.py
+++ Lab_6/client_sop.py
@@ -1,11 +1,12 @@
 import paho.mqtt.client as mqtt
-import argparse
 import threading
+import argparse
+import random
 import time
 import uuid
 import json
 
-TIMEOUT_LIMIT = 600
+TIMEOUT_LIMIT = 60
 QTDE_CLIENTS = 2
 
 def parse_args() -> tuple[int, int]:
@@ -17,7 +18,8 @@ def parse_args() -> tuple[int, int]:
     return args.port, args.host
 
 class Client:
-    uuid = uuid.uuid4()
+    uuid = str(uuid.uuid4())
+    votes = []
 
     def __init__(self, port, host):
         self.port = port
@@ -46,11 +48,11 @@ class Client:
 
     def send_init(self):
         start = time.time()
-        msg = json.dumps({"ClientID": str(self.uuid)})
+        msg = json.dumps({"ClientID": self.uuid})
 
-        while time.time() - start < TIMEOUT_LIMIT and len(self.known_clients) < QTDE_CLIENTS:
+        while time.time() - start < TIMEOUT_LIMIT or len(self.known_clients) < QTDE_CLIENTS:
             self.client.publish("sd/init", msg)
-            time.sleep(60)
+            time.sleep(10)
 
     def on_message(self, client, userdata, msg):
         if msg.topic == "sd/init":
@@ -65,7 +67,6 @@ class Client:
         elif msg.topic == "sd/challenge":
             self.on_challenge(msg.payload)
 
-        print(msg.topic+" "+str(msg.payload))
 
     def on_init(self, msg):
         client_id = json.loads(msg)['ClientID']
@@ -80,6 +81,17 @@ class Client:
         print('mensagem de init', msg)
 
     def on_voting(self, msg):
+        msg_json = json.loads(msg)
+        
+        if msg_json['ClientID'] not in self.votes:
+            self.votes.append(msg_json)
+
+        if len(self.votes) == QTDE_CLIENTS:
+            self.client.unsubscribe("sd/voting")
+            print('Votação encerrada')
+            
+            self.elect_leader()
+
         print('mensagem de voto', msg)
 
     def on_result(self, msg):
@@ -90,9 +102,50 @@ class Client:
 
     def send_voting(self):
         vote = uuid.uuid4()
-        vote_msg = json.dumps({"ClientID": str(self.uuid), "VoteID": str(vote)})
+        vote_msg = json.dumps({"ClientID": self.uuid, "VoteID": str(vote)})
 
         self.client.publish("sd/voting", vote_msg)    
+
+    def elect_leader(self):
+        # finds vote of biggest value, if there is a tie, client ID is used as tie breaker
+        max_vote = max(self.votes, key=lambda x: (x['VoteID'], x['ClientID']))
+        print()
+        print(max_vote)
+        print()
+        if max_vote['ClientID'] == self.uuid:
+            self.client.unsubscribe(["sd/result", "sd/challenge"])
+            
+            self.client.subscribe("sd/solution")
+            self.challenges = []
+
+            self.show_menu()
+        
+    
+    def show_menu(self):
+        print("Escolha uma das opções abaixo:")
+        print("1 - Iniciar desafio")
+        print("2 - Encerrar")
+
+        option = int(input("Opção: "))
+        if option == 1:
+            self.newTransaction()
+        elif option == 2:
+            self.client.disconnect()
+
+
+    def newTransaction(self):
+        difficulty = random.randint(1, 6)
+        print(f"New challenge: lvl {difficulty}")
+        
+        challenge = {
+            "transactionID": len(self.challenges),
+            "challenge": difficulty,
+            "solution": None,
+            "winner": 0
+        }
+        self.challenges.append(challenge)
+
+        self.client.publish("sd/challenge", json.dumps({"TransactionID": challenge["transactionID"], "Challenge": challenge["challenge"]}))
 
 
 if __name__ == "__main__":
